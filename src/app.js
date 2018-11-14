06855e90c27d315d1f2ec759a641297ff4ef2d3e
Emmanuel <emmanuel@kubofinanciero.com>
2018-11-14 15:02:59 -0600

    Extract cropper to file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
-import Cropper from 'cropperjs';
+import { buildCropper } from './cropper';
 import './style.css';
 
 import { buildButtons } from './buttons';
 
 window.onload = () => {
   console.log('page has been loaded');
-  const image = document.getElementById('app');
-  const cropper = new Cropper(image, { aspectRatio: 16 / 9 });
-
+  buildCropper('app');
   const container = document.querySelector('.image-container');
-  buildButtons(container, cropper);
+  buildButtons(container);
 };
--- src/buttons/index.js
+++ src/buttons/index.js
@@ -1,4 +1,5 @@
-/* eslint-disable import/prefer-default-export */
+import { getCropper, destroyCropper, buildCropper } from '../cropper';
+
 const singleButton = (value, id) => {
   const button = document.createElement('INPUT');
   button.value = value;
@@ -8,42 +9,44 @@ const singleButton = (value, id) => {
   return button;
 };
 
-const rotateRightButton = (cropper) => {
+const rotateRightButton = () => {
   const rightButton = singleButton('Rotate Right', 'rotate-right-button');
   rightButton.addEventListener('click', (e) => {
     e.preventDefault();
     e.stopPropagation();
-    cropper.rotate(45);
+    getCropper().rotate(45);
   });
   return rightButton;
 };
 
-const rotateLeftButton = (cropper) => {
+const rotateLeftButton = () => {
   const leftButton = singleButton('Rotate Left', 'rotate-left-button');
   leftButton.addEventListener('click', (e) => {
     e.preventDefault();
     e.stopPropagation();
-    cropper.rotate(-45);
+    getCropper().rotate(-45);
   });
   return leftButton;
 };
 
-const cropButton = (cropper) => {
+const cropButton = () => {
   const save = singleButton('Crop', 'crop-button');
   save.addEventListener('click', (e) => {
     e.preventDefault();
     e.stopPropagation();
-    const data = cropper.getCroppedCanvas();
-    console.log('cropped canvas', data.toDataURL());
+    const data = getCropper().getCroppedCanvas();
+    document.getElementById('app').src = data.toDataURL();
+    destroyCropper();
+    buildCropper('app');
   });
   return save;
 };
 
-export const buildButtons = (container, cropper) => {
+export const buildButtons = (container) => {
   const buttonsContainer = document.createElement('DIV');
   buttonsContainer.className = 'buttons-container';
-  buttonsContainer.appendChild(rotateRightButton(cropper));
-  buttonsContainer.appendChild(rotateLeftButton(cropper));
-  buttonsContainer.appendChild(cropButton(cropper));
+  buttonsContainer.appendChild(rotateRightButton());
+  buttonsContainer.appendChild(rotateLeftButton());
+  buttonsContainer.appendChild(cropButton());
   container.appendChild(buttonsContainer);
 };
