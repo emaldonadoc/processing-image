a507a9d8e05e5650f8bbeaf57b5e4ab085d6f56e
Emmanuel <emmanuel@kubofinanciero.com>
2018-11-16 15:27:44 -0600

    .i.

--- src/app.js
+++ src/app.js
@@ -3,15 +3,21 @@ import './style.css';
 
 import { buildButtons } from './buttons';
 
-export default ({ imgUrl, saveCallback, deleteCallback }) => {
+export default ({
+  container,
+  imgUrl,
+  saveCallback,
+}) => {
   const imageContainer = document.createElement('DIV');
   imageContainer.className = 'image-container';
   const img = document.createElement('IMG');
   img.src = imgUrl;
   img.id = 'app';
+  img.onload = (e) => {
+    console.log('Image has been loaded', e);
+    buildCropper('app');
+    buildButtons(imageContainer, saveCallback);
+  };
   imageContainer.appendChild(img);
-  document.body.appendChild(imageContainer);
-
-  buildCropper('app');
-  buildButtons(imageContainer);
+  document.querySelector(container).appendChild(imageContainer);
 };
--- src/buttons/index.js
+++ src/buttons/index.js
@@ -9,6 +9,7 @@ const fontAwesomeMap = {
   'rotate-right-button': 'repeat',
   'rotate-left-button': 'undo',
   'crop-button': 'crop',
+  'save-button': 'check',
 };
 
 const singleButton = (id) => {
@@ -43,22 +44,45 @@ const rotateLeftButton = () => {
 };
 
 const cropButton = () => {
-  const save = singleButton('crop-button');
-  save.addEventListener('click', (e) => {
+  const crop = singleButton('crop-button');
+  crop.addEventListener('click', (e) => {
     e.preventDefault();
     e.stopPropagation();
     document.getElementById('app').src = getCroppedB64FromCanvas();
     destroyCropper();
     buildCropper('app');
   });
+  return crop;
+};
+
+const saveButton = (saveCallback) => {
+  const save = singleButton('save-button');
+  save.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const b64ImageCrooped = getCroppedB64FromCanvas();
+    saveCallback(b64ImageCrooped);
+  });
   return save;
 };
 
-export const buildButtons = (container) => {
-  const buttonsContainer = document.createElement('DIV');
-  buttonsContainer.className = 'buttons-edition-container';
-  buttonsContainer.appendChild(rotateLeftButton());
-  buttonsContainer.appendChild(rotateRightButton());
-  buttonsContainer.appendChild(cropButton());
-  container.prepend(buttonsContainer);
+const editionButtons = () => {
+  const editionButtonsContainer = document.createElement('DIV');
+  editionButtonsContainer.className = 'edition-buttons-container';
+  editionButtonsContainer.appendChild(rotateLeftButton());
+  editionButtonsContainer.appendChild(rotateRightButton());
+  editionButtonsContainer.appendChild(cropButton());
+  return editionButtonsContainer;
+};
+
+const actionsButton = (saveCallback) => {
+  const actionsButtonsContainer = document.createElement('DIV');
+  actionsButtonsContainer.className = 'action-buttons-container';
+  actionsButtonsContainer.appendChild(saveButton(saveCallback));
+  return actionsButtonsContainer;
+};
+
+export const buildButtons = (container, saveCallback) => {
+  container.prepend(editionButtons());
+  container.appendChild(actionsButton(saveCallback));
 };
